212dd896c95eb3c120b039ace521e77fafa3df4d
MuhammadBilal <bilalzafarr39@gmail.com>
2021-12-15 16:00:36 +0500

    first commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const TodoItem = ({ todo, deleteTodo }) => {
+  return (
+    <p onDoubleClick={() => deleteTodo(todo.id)} style={{ cursor: "pointer" }}>
+      {todo.title}
+    </p>
+  );
+};
+
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired,
+  deleteTodo: PropTypes.func.isRequired,
+};
+
+export default TodoItem;
